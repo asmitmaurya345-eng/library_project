da7f0e7928d11df232071e73c5930cc63c599ee6
Asmit Maurya <asmitmaurya345@gmail.com>
2026-04-25 14:37:49 +0530

    Delete findbook.py

--- findbook.py
+++
@@ -1,7 +0,0 @@
-from database import l1
-def fbook():
-    b=input("Enter book name or book number of the book you want to find:")
-    for x,y in l1.items():
-        if x==b or y==b:
-            print("We have the book u are looking for:",y)
-            c=input("enter to continew")
